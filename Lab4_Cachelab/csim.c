da55656c8fb1920c3ef73f9bdf710fb9458a3625
Ryan Beck <ryan.beck1820@gmail.com>
2025-11-12 22:48:07 -0700

    Cache structures and memory allocation/deallocation

--- Lab4_Cachelab/csim.c
+++ Lab4_Cachelab/csim.c
@@ -9,15 +9,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// DEBUG AND HELPER FUNCTIONS
 void printHelp();
 void printError();
 void printArgs();
 
+// CACHE SIMULATION FUNCTIONS
+cache* createCache(int s, int E, int b);
+void freeCache(cache* c);
+
+// CACHE SIMULATION VARIABLES AND TYPES
 int indexBits;
 int lineCount;
 int offsetBits;
 char traceFile[32];
 
+typedef struct {
+    bool valid;
+    int tag;
+    int block;
+} line;
+
+typedef struct {
+    line* lines;
+} set;
+
+typedf struct {
+    set* sets;
+    int s;
+    int E;
+    int b;
+} cache;
+
+
+// MAIN FUNCTION CODE
 int main(int argc, char* argv[])
 {
     // Flag check
@@ -41,6 +66,7 @@ int main(int argc, char* argv[])
     }
 
     //Flag check succeeded
+    // Collect input argments in global variables
     indexBits = atoi(argv[2]);
     lineCount = atoi(argv[4]);
     offsetBits = atoi(argv[6]);
@@ -52,14 +78,85 @@ int main(int argc, char* argv[])
     printArgs();
 
     // Generate Cache Table
+    cache myCache = creatCache(indexBits, lineCount, offsetBits);
 
     // Iterate through traceFile lines
 
-    // Finish with this
+    // Deallocate cache
+    freeCache(myCache);
+
+    // Get summary for grading
     printSummary(0, 0, 0);
     return 0;
 }
 
+/*
+ * Function: 	createCache
+ * Input:	int <s> - number of set index bits
+ * 		int <E> - number of lines per set (associativity)
+ * 		int <b> - number of block offset bits
+ * Output:	cache*	- pointer to cache structure
+ * Description:
+ * Take input arguments for cache parameters <s>, <E>, <b> and
+ * dynamically allocate the simulation cache, with its respective
+ * sets and lines. Initialize each line as
+ * 	valid	= false
+ * 	tag	= -1
+ * 	block	= 0 
+ */
+cache* createCache(int s, int E, int b) {
+    cache* c = malloc(sizeof(cache));
+    c->s = s;
+    c->E = E;
+    c->b = b;
+
+    int S = 1 << s;
+    c->sets = malloc(S * sizeof(set));
+
+    for (int i=0; i < S; i++) {
+    	c->sets[i].lines = malloc(E * sizeof(line));
+	for (int j = 0; j < E; j++) {
+	    c->sets[i].lines[j].valid = false;
+	    c->sets[i].lines[j].tag = -1;
+	    c->sets[i].lines[j].block = 0;
+	}
+    }
+
+    return c;
+}
+
+
+/*
+ * Function:	freeCache
+ * Input:	cahe* <c> - pointer to cache structure
+ * Output:	void
+ * Description:
+ * Take input argment <c> and deallocate memory used by the cache.
+ * Deallocation occurs in the following order
+ * 	> lines
+ * 	> sets
+ * 	> cache
+ */
+void freeCache(cache* c) {
+    int S = 1 << c->s;
+    for (int i = 0; i < S; i++) {
+    	free(c->sets[i].lines);
+    }
+    free(c->sets);
+    free(c);
+}
+
+/*
+ * Function:	printArgs
+ * Input:	void
+ * output:	void
+ * Description:
+ * (DEBUG) Print the values of the following global variables to the console.
+ * 	> indexBits
+ * 	> lineCount
+ * 	> offsetBits
+ * 	> traceFile
+ */
 void printArgs() {
    printf("Set index bits:    %d\n", indexBits);
    printf("Lines per set:     %d\n", lineCount);
@@ -67,6 +164,14 @@ void printArgs() {
    printf("Trace file name:   %s\n", traceFile);
 }
 
+/*
+ * Function:	printHelp
+ * Input:	void
+ * Output:	void
+ * Description:
+ * (HELPER) print the usage message for the csim program. Used when input arguments
+ * are malformed, or the -h flag is included.
+ */
 void printHelp() {
 // Print help message
    printf("Usage: ./csim -h -s <num> -E <num> -b <num> -t <file>\n");
@@ -80,8 +185,13 @@ void printHelp() {
    printf("  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
 }
 
-
-
+/*
+ * Function:	printError
+ * Input:	void
+ * Output:	void
+ * Description:
+ * (HELPER) print the error message for malformed input arguments.
+ */
 void printError() {
    printf("./csim: Missing required command line argument\n");
 }
