7ab559df25005c58ab5772f1cf2474f3e5f2e4d3
Joshua <christensenjl123@gmail.com>
2025-11-18 15:17:07 -0700

    ready to submit

--- Lab4_Cachelab/trans.c
+++ Lab4_Cachelab/trans.c
@@ -1,7 +1,5 @@
 /* 
  * trans.c - Matrix transpose B = A^T
- * Ryan Beck A02237765
- * Josh Christensen
  *
  * Each transpose function must have a prototype of the form:
  * void trans(int M, int N, int A[N][M], int B[M][N]);
@@ -12,12 +10,10 @@
 #include <stdio.h>
 #include "cachelab.h"
 
-#define BLOCK 4
-
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 void trans(int M, int N, int A[N][M], int B[M][N]);	// Basic transpose function
 void zigzag_transpose(int N_start, int M_start,int M, int N, int A[N][M], int B[M][N], int block);
-
+void case_transpose(int M, int N, int A[N][M], int B[M][N]);
 
 /* 
  * transpose_submit - This is the solution transpose function that you
@@ -29,14 +25,7 @@ void zigzag_transpose(int N_start, int M_start,int M, int N, int A[N][M], int B[
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-    int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }
+    case_transpose(M,N,A,B);
 }
 
 /* 
@@ -311,8 +300,8 @@ void zigzag_transpose(int N_start, int M_start, int M, int N, int A[N][M], int B
 char block_zigzag_trans_2_desc[] = "block based zigzag access transpose function";
 void block_zigzag_trans_2(int M, int N, int A[N][M], int B[M][N])
 {
-    const int block_i = 4;
-    const int block_j = 4;
+    const int block_i = 8;
+    const int block_j = 8;
     int i = 0;
     int j = 0;
     int	ii, jj;
@@ -400,17 +389,190 @@ void block_zigzag_trans_2(int M, int N, int A[N][M], int B[M][N])
     }
 }
 
+
 /*
-*	function GPT_transpose
+*	function block_snake_trans
+*	basic blocking snake function made to run with asymetric matricies with any matrix size
 *	best results found when block = 8
+*/
+char block_snake_trans_desc[] = "block based transpose function";
+void block_snake_trans(int M, int N, int A[N][M], int B[M][N])
+{
+    const int block_i = 4;
+    const int block_j = 4;
+    int i, j, ii, jj, tmp;
+    int M_tail = M % block_i;
+    int N_tail = N % block_j;
+    M -= M_tail;
+    N-= N_tail;
+    
+    // transpose blocks that divide evenly
+    for (i = 0; i < N; i += block_i)
+    {
+	for (j = 0; j < M; j += block_j)
+	{
+	    for (ii = i; ii < i + block_i; ii++)
+	    {
+		if (ii % 2) // if odd row, go backwards
+		{
+		    for (jj = j + block_j - 1; jj >= j; jj--)
+		    {
+			tmp = A[ii][jj];
+			B[jj][ii] = tmp;
+		    }
+		}
+		else // if even row, go forwards
+		{
+		    for (jj = j; jj < j + block_j; jj++)
+		    {
+			tmp = A[ii][jj];
+			B[jj][ii] = tmp;
+		    }
+		}
+	    }
+	}
+    }
+
+    // transpose tail manually
+    for (i = N; i < N+N_tail; i++)
+    {
+	for (j = 0; j < M+M_tail; j++)
+	{
+	    tmp = A[i][j];
+	    B[j][i] = tmp;
+	}
+    }
+    for (j = M; j < M+M_tail; j++)
+    {
+	for (i = 0; i < N; i++)
+	{
+	    tmp = A[i][j];
+	    B[j][i] = tmp;
+	}
+    }
+}
+
+
+
+/*
+*	function block_square_diag_trans
+*	basic blocking snaking function that uses special handling for the diagonal blocks. Only works with square block sizes
+*	still works for asymetric matricies
+*/
+char block_square_diag_trans_desc[] = "block based diagonal handling transpose function";
+void block_square_diag_trans(int M, int N, int A[N][M], int B[M][N])
+{
+    // choose block size
+    int b;
+    if ( M == N && M == 32)
+    {
+	b = 8;
+    }
+    else
+    {
+	b = 4;
+    }
+    const int block = b;
+    int i, j, ii, jj;
+    int M_tail = M % block;
+    int N_tail = N % block;
+    M -= M_tail;
+    N -= N_tail;
+    if( M > N) // create a square submatrix
+    {
+	M_tail += (M - N);
+	M -= (M - N);
+    }
+    if( N > M)
+    {
+	N_tail += (N - M);
+	N -= (N - M);
+    }
+    
+    // transpose blocks that divide evenly
+    for (i = 0; i < N; i += block)
+    {
+	for (j = 0; j < M; j += block)
+	{
+	    if (i != j) // transpose non-diagonal blocks first
+	    {
+		for (ii = i; ii < i + block; ii++)
+            	{
+                    if (ii % 2) // if odd row, go backwards
+		    {
+		    	for (jj = j + block - 1; jj >= j; jj--)
+		    	{
+			    int tmp = A[ii][jj];
+			    B[jj][ii] = tmp;
+		    	}
+		    }
+		    else // if even row, go forwards
+		    {
+		    	for (jj = j; jj < j + block; jj++)
+		    	{
+			    int tmp = A[ii][jj];
+			    B[jj][ii] = tmp;
+		    	}
+		    }
+                }
+	    }
+	}
+    }
+    
+    
+    int temp[block][block];
+    // transpose the diagonal blocks in two steps
+    for (i = 0; i < N; i += block) 
+    {
+        // Step A -> temp
+        for (ii = 0; ii < block && (i + ii) < N; ii++) {
+            for (jj = 0; jj < block && (i + jj) < M; jj++) {
+                temp[jj][ii] = A[i + ii][i + jj];   // transpose into temp
+            }
+        }
+
+        // Copy temp -> B
+        for (ii = 0; ii < block && (i + ii) < N; ii++) {
+            for (jj = 0; jj < block && (i + jj) < N; jj++) {
+                B[i + jj][i + ii] = temp[jj][ii];    // write from temp
+            }
+        }
+    }
+
+
+
+    // transpose tail manually
+    for (i = N; i < N+N_tail; i++)
+    {
+	for (j = 0; j < M+M_tail; j++)
+	{
+	    B[j][i] = A[i][j];
+	}
+    }
+    for (j = M; j < M+M_tail; j++)
+    {
+	for (i = 0; i < N; i++)
+	{
+	    B[j][i] = A[i][j];
+	}
+    }
+}
+
+
+
+/*
+*	function case_transpose
+*	a blocking function that takes each submission case and uses the optimal algorithm to get the transpose 
+*	For the 64x64 case the registers are loaded 8 at a time and then stored 8 at a time
+*	best results found when block = 8 for 32x32 and 64x64
 */
 #define BLOCK_32 8
 #define BLOCK_64 8
 #define BLOCK_GENERIC 16
 
-char GPT_transpose_desc[] = "GPT Generated Transpose";
+char case_transpose_desc[] = "GPT Generated Transpose";
 
-void GPT_transpose(int M, int N, int A[N][M], int B[M][N])
+void case_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     // -----------------------------
     // Case 1: 32x32
@@ -446,7 +608,7 @@ void GPT_transpose(int M, int N, int A[N][M], int B[M][N])
             for (int jj = 0; jj < 64; jj += BLOCK_64) {
 	        
 		int a0, a1, a2, a3, a4, a5, a6, a7; // 8 Local variables
-
+			// load in 8 values at a time
 		for (int i=0; i<4; i++) {
 		    a0 = A[ii+i][jj+0];
 		    a1 = A[ii+i][jj+1];
@@ -456,7 +618,7 @@ void GPT_transpose(int M, int N, int A[N][M], int B[M][N])
 		    a5 = A[ii+i][jj+5];
 		    a6 = A[ii+i][jj+6];
 		    a7 = A[ii+i][jj+7];
-
+			// Then write the 8 values at the same time
 		    B[jj+0][ii+i] = a0;
 		    B[jj+1][ii+i] = a1;
 		    B[jj+2][ii+i] = a2;
@@ -528,6 +690,14 @@ void GPT_transpose(int M, int N, int A[N][M], int B[M][N])
 }
 
 
+
+
+
+
+
+
+
+
 /*
  * registerFunctions - This function registers your transpose
  *     functions with the driver.  At runtime, the driver will
@@ -542,9 +712,9 @@ void registerFunctions(void)
 
     /* Register any additional transpose functions */
     //registerTransFunction(block_trans, block_trans_desc);
-    //registerTransFunction(block_asym_trans, block_asym_trans_desc);
-    //registerTransFunction(block_zigzag_trans_2, block_zigzag_trans_2_desc);
-    registerTransFunction(GPT_transpose, GPT_transpose_desc);
+    registerTransFunction(block_asym_trans, block_asym_trans_desc);
+    registerTransFunction(block_square_diag_trans, block_square_diag_trans_desc);
+    registerTransFunction(case_transpose, case_transpose_desc);
 }
 
 /* 
